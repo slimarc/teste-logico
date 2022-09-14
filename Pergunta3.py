ad76bb24e7924b7155e89cad1ae373e041e0ef62
Ricardo Lima <sricardolimaa@gmail.com>
2022-09-14 09:36:11 -0300

    finalizado

--- Pergunta3.py
+++ Pergunta3.py
@@ -1,5 +1,21 @@
-def encontrar_soma(valor, true):
+from itertools import combinations
 
-    return valor, true 
+def encontrar_combinacao(soma):
+    a = True
+    b = False
+    for i in (2, len(vet)):
+        for comb in combinations(vet, i):
+            if (sum(comb) == x):
+                print("Existe a soma no vetor: ")
+                print (comb,'= ', x)
+                return a
+        if(sum(comb) != x):
+            print("Nao existe a soma no vetor")
+            return b
+    return soma
+    
+    
+vet = [1, 15, 2, 7, 2, 5, 7, 1, 4]
+x = int(input("Insira o valor de x para verificar se a soma existe dentro do arrey"))
 
-vet = [1, 15, 2, 7, 2, 5, 7, 1, 4] 
+encontrar_combinacao(x)
